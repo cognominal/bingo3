f163442fc831c7b0fd20aafcf482adbb730df999
Stéphane Payrard <cognominal@gmail.com>
2023-12-09 19:47:24 +0100

    got a split pane

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let data;
+	import { SplitPane } from '@rich_harris/svelte-split-pane';
+    const dividerColor = 'black';
+  const dividerThickness = '20px';
+  
+</script>
+
+<SplitPane type="horizontal"  min="25%" max="50%" pos="50%" --color={dividerColor}
+--thickness={dividerThickness} 
+>
+    <div slot="a">
+        <section>Left</section>
+    </div>
+    <div slot="b">
+        <section>Right</section>
+    </div>
+</SplitPane>
+
+<style>
+    section {
+        padding: 1em;
+        margin: 1em;
+        border: 1px solid #ccc;
+        border-radius: 3px;
+        min-height: 200px; /* Set the minimum width here */
+
+    }
+</style>
